dd4ea6864c5337ff3d20a5ad2dd27a4ce629b850
fab fab <fab63181@gmail.com>
2025-10-31 00:14:42 +0000

    add normalization

--- models/staging/localbike_database/stg_localbike_database__categories.sql
+++ models/staging/localbike_database/stg_localbike_database__categories.sql
@@ -1,5 +1,5 @@
 select
-  CAST(category_id AS INTEGER) AS category_id,
-  CAST(category_name AS STRING) AS category_name
+  CAST(category_id AS INTEGER)  as category_id,
+  CAST(category_name AS STRING) as category_name
 from
   {{ source('localbike_database','categories') }}
